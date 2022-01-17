0d8229b9344044bfb70dc92a4d7a581cb57b8414
zengboi <mithileshgautam666@gmail.com>
2022-01-17 19:06:04 +0530

    database model created

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
+import imp
+from uuid import uuid4
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null= True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank = True)
+    created = models.DateTimeField(auto_now_add= True)
+    id = models.UUIDField(default= uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
