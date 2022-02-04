376a7366ee470be1e5c053286e431ba2ae5bf1c8
zengboi <mithileshgautam666@gmail.com>
2022-02-04 10:10:59 +0530

    CRUD Functionality added in forms style

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Project, Review, Tag
+from .forms import ProjectForm
 # Create your views here.
 
 def projects(request):
@@ -18,3 +19,39 @@ def project(request, pk):
         'project':projectObj,
     }
     return render(request, 'projects/single-project.html', context)
+
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form':form}
+    return render(request, 'projects/project_forms.html', context)
+
+def updateProject(request,pk):
+    project = Project.objects.get(id = pk)
+    form = ProjectForm(instance= project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    
+    context = {
+        'form':form,
+    }
+    return render(request, 'projects/project_forms.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id = pk)
+
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'project':project}
+    return render(request, 'projects/delete_project.html', context)
