945e26e1e4b5dad0c8f53f7d7ae18b3481328f34
zengboi <mithileshgautam666@gmail.com>
2022-02-04 16:39:12 +0530

    added style in project-form,projects page,single-project

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
@@ -7,3 +8,14 @@ class ProjectForm(ModelForm):
         model = Project
         # fields = "__all__"
         exclude = ['vote_total', 'vote_ratio']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs.update({'class':'input input--text','placeholder':'Enter Title'})
+        self.fields['description'].widget.attrs.update({'class':'input input--textarea--sm','placeholder':'Enter Description'})
+        self.fields['demo_link'].widget.attrs.update({'class':'input input--text','placeholder':'Enter Live Demo Link'})
+        self.fields['source_link'].widget.attrs.update({'class':'input input--text','placeholder':'Enter Code Link'})
