8b69bf037b55241b4f468fcbd98a636360b26ce6
zengboi <mithileshgautam666@gmail.com>
2022-02-05 16:24:17 +0530

    modified profile,developers,) template page

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('user-profile/<str:pk>/', views.userProfile, name='user-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,24 @@
+from pydoc import describe
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
+
 def profile(request):
-    return render(request, 'users/profile.html')
+    profiles = Profile.objects.all()
+    context = {
+        'profiles':profiles,
+    }
+    return render(request, 'users/profile.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id = pk)
+    topskills = profile.skill_set.exclude(description__exact = '')
+    otherskills = profile.skill_set.filter(description='')
+    
+    context = {
+        'profile':profile,
+        'topskill':topskills,
+        'otherskill': otherskills,
+    }
+    return render(request, 'users/user-profile.html', context)
